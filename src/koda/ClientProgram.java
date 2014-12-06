4d0fa718fde5c079eb38a987e2170553c79232d2
smahbod <smahbod@ucsd.edu>
2014-12-05 22:24:28 -0800

    Got console-based echo working

--- src/koda/ClientProgram.java
+++ src/koda/ClientProgram.java
@@ -1,5 +1,10 @@
 package koda;
 
+import java.net.InetAddress;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -7,24 +12,52 @@ import com.esotericsoftware.kryonet.Listener;
 public class ClientProgram extends Listener {
 
 	static Client client;
-	static String ip = "128.54.228.219";
-	static int tcpPort = 27961;
+	//static String ip = "128.54.228.219";
+	static int tcpPort = 27960;
 	static int udpPort = 27961;
 	
 	static boolean messageReceived = false;
+	static boolean chatMessageReceived = false;
 	
 	public static void main(String[] args) throws Exception {
 		client = new Client();
-		client.getKryo().register(PacketMessage.class);
+		registerPackets();
 		client.start();
-		client.connect(5000, ip, tcpPort, udpPort);
+		InetAddress address = client.discoverHost(udpPort, 5000);
+		client.connect(5000, address, tcpPort, udpPort);
+		//client.connect(5000, ip, tcpPort, udpPort);
 		client.addListener(new ClientProgram());
 		
 		while (!messageReceived) {
 			Thread.sleep(1000);
 		}
 		
-		System.out.println("Client received the message. Exiting");
+		
+		
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		System.out.print("Enter message: ");
+		while (!(input = scan.nextLine()).equals("done")) {
+			chatMessageReceived = false;
+			ChatMessage pkt = new ChatMessage();
+			pkt.message = input;
+			client.sendTCP(pkt);
+			waitForResponse();
+			System.out.print("Enter message: ");
+		}
+		scan.close();
+	}
+	
+	public static void registerPackets() {
+		client.getKryo().register(PacketMessage.class);
+		client.getKryo().register(ChatMessage.class);
+		client.getKryo().register(StatusMessage.class);
+	}
+	
+	private static void waitForResponse() throws InterruptedException {
+		while (!chatMessageReceived) {
+			Thread.sleep(1);
+		}
 	}
 	
 	@Override
@@ -33,6 +66,22 @@ public class ClientProgram extends Listener {
 			PacketMessage message = (PacketMessage) p;
 			System.out.println("Server says: " + message.message);
 			messageReceived = true;
+		} else if (p instanceof ChatMessage) {
+			ChatMessage pkt = (ChatMessage) p;
+			System.out.println("Server says: " + pkt.message);
+			chatMessageReceived = true;
+		} else if (p instanceof StatusMessage) {
+			StatusMessage pkt = (StatusMessage) p;
+			switch (pkt.status) {
+			case StatusMessage.KICKED:
+				JOptionPane.showMessageDialog(null, "You were kicked!", "Kicked", JOptionPane.INFORMATION_MESSAGE);
+				System.exit(0);
+				break;
+			case StatusMessage.SERVER_SHUTTING_DOWN:
+				JOptionPane.showMessageDialog(null, "Server shutting down.", "Server shutting down", JOptionPane.INFORMATION_MESSAGE);
+				System.exit(0);
+				break;
+			}
 		}
 	}
 }
